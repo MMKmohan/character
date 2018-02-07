013ac489767b98be252ed3f94022f287ae990315
MOHAN KUMAR <mohan.m13579@gmail.com>
2018-02-07 16:42:36 +0530

    7\2\midarray.C
    to find middle value using array

---
+++ 7\2\midarray.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int a[50],i,k,size;
+printf("\n enter the size of the array:");
+scanf("%d",&size);
+for(i=0;i<size;i++)
+{
+scanf("%d",&a[i]);
+}
+k=size/2;
+printf("\n the middle element is %d",a[k]);
+}
