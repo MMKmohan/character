0a5cab1a0478a85a933532e9a6e55e44ce158c00
MOHAN KUMAR <mohan.m13579@gmail.com>
2018-02-12 16:08:47 +0530

    12\2\fibonnaci.C
    to enter fibonnaci series

---
+++ 12\2\fibonnaci.C
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void main() 
+{
+int n;
+printf("enter a number");
+scanf("%d",&n);
+int next,first=0,second=1,i;
+for(i=0;i<n;i++)
+{
+if(n==1)
+{
+next=n;
+}
+else
+{
+first=second;
+second=next;
+next=first+second;
+}
+printf("%d ",next);
+}
+}
