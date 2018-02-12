9042192c8088137d0241ae5968ffb3732d825f52
MOHAN KUMAR <mohan.m13579@gmail.com>
2018-02-12 16:14:10 +0530

    12\2\string.C
    to print the world number of times

---
+++ 12\2\string.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<string.h>
+void main()
+{
+char s[100];
+int n,i;
+printf("enter the word");
+scanf("%s",s);
+printf("ebnter a number");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+printf("\n %s",s);
+}
+}
