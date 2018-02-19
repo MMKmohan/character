781651cfd79d21160d2ead6d37d7dc35820236a1
MOHAN KUMAR <mohan.m13579@gmail.com>
2018-02-19 16:34:46 +0530

    19\2\alpnum.C
    to check wheather it contains both alphabets&numbers

---
+++ 19\2\alpnum.C
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdio.h>
+void main()
+{
+int i;
+char s[100],a_count=0,n_count=0;
+gets(s);
+for(i=0;s[i]!='\0';i++)
+{
+if((s[i]>='a' && s[i]<='z') || (s[i]>='A' && s[i]<='Z'))
+{
+a_count++;
+}
+if(s[i]>='0' && s[i]<='9')
+{
+n_count++;
+}
+}
+if((a_count && n_count)>0)
+printf("yes,it contains both alphabets&numbers");
+else
+printf("no,it does't contains both alphabets&numbers");
+}
