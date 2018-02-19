536f3ea628439f52fc360c17cdc258083a05c27a
MOHAN KUMAR <mohan.m13579@gmail.com>
2018-02-19 16:02:20 +0530

    19\2\sum.C
    to find the sum of two numbers

---
+++ 19\2\sum.C
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main() 
+{
+int n1,n2,sum;
+printf("\n enter the first number:");
+scanf("%d",&n1);
+printf("\n enter the second number:");
+scanf("%d",&n2);
+sum=n1+n2;
+printf("the sum of 2 numbers are%d",sum);
+}
