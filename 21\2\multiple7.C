f1679c9f4c168eaaf73c00e4fcef33c5740112d3
MOHAN KUMAR <mohan.m13579@gmail.com>
2018-02-21 16:06:29 +0530

    21\2\multiple7.C
    to check wheather the given number is multiple of 7 or not

---
+++ 21\2\multiple7.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("enter a number to chek it is a multiple of 7 or not  ");
+scanf("%d",&a);
+if(a%7==0)
+printf("\n Multiple of 7");
+else
+printf("\n Not Multiple of 7");  
+}
