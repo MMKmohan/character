0c27d8c3bad370b3224401fe0f9faea88bb082d0
MOHAN KUMAR <mohan.m13579@gmail.com>
2018-03-02 16:13:48 +0530

    2\3\lbh.C
    to find the volume

---
+++ 2\3\lbh.C
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main() 
+{
+int l,b,h;
+printf("enter length,breath & height:   ");
+scanf("%d %d %d",&l,&b,&h);
+int volume,tsa;
+volume=l*b*h;
+printf("\nVOLUME=%d",volume);
+
+}
