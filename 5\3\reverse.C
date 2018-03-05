97775a962355de7072865a91753c32d31ae5e564
MOHAN KUMAR <mohan.m13579@gmail.com>
2018-03-05 15:50:19 +0530

    5\3\reverse.C
    to print the number in reverse

---
+++ 5\3\reverse.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main() 
+{
+int n,remainder,reverse=0;
+int result;
+printf("\n enter the number:  ");
+scanf("%d",&n);
+while(n!=0)
+{
+remainder=n%10;
+reverse=reverse*10+remainder;
+n=n/10;
+}
+printf("\n%d",reverse);
+}
