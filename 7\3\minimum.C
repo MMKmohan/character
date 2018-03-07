6c5d61b03c6d75a3cf0b2fed42b1b494fed67402
MOHAN KUMAR <mohan.m13579@gmail.com>
2018-03-07 16:03:18 +0530

    7\3\minimum.C
    to find the minimum among the given numbers

---
+++ 7\3\minimum.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+int a[10],min=0,i,n;
+min=a[0];
+for(i=0;i<10;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=1;i<10;i++)
+{
+if(a[i]<min)
+min=a[i];
+}
+printf("%d",min);
+}
